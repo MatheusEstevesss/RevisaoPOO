d3a906405f0b492f2ed3ad48e078b9b5a5a94da3
MatheusEstevesss <matheusesantos@gmail.com>
2021-05-14 16:31:26 -0300

    Adicionando exemplo Auto Properties

---
+++ AutoProperties/AutoProperties/Produto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace AutoProperties
+{
+    class Produto
+    {
+        private string _nome; //nao fazemos o AutoProp pois o nome possui uma lógica particular do nosso programa
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }//Exemplo de auto propertie, faz a mesma coisa que a forma antiga porem de uma maneira resumida ATALHO prop para formar isso mais rápido
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco)
+        {
+            _nome = nome;
+            Preco = preco;
+        }
+
+        //Exemplo Properties - Agora isso nos possibilita que no programa principal nós simplemente coloquemos p.Nome (como c fosse um atributo) 
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;//Este value substitui o parâmetro que estaria sendo criado no método Set
+                }
+            }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AddProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoveProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ AutoProperties/AutoProperties/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AutoProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "TV 4K";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.Quantidade);
+        }
+    }
+}
