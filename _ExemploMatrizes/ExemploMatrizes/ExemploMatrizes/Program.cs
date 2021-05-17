08b652ab6dd7695efe820c3d29433afc52a867c6
MatheusEstevesss <matheusesantos@gmail.com>
2021-05-17 20:49:09 -0300

    Adding examples of matrix

---
+++ _ExemploMatrizes/ExemploMatrizes/ExemploMatrizes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ExemploMatrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+
+            Console.Write("Tamanho da matrix: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < n; i++) {
+
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++) {
+                    matrix[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine();
+            Console.Write("Main diagonal: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{matrix[i,i]} ");
+            }
+
+            Console.WriteLine();
+            Console.Write("Negative numbers = ");
+            foreach (int item in matrix)
+            {                
+                if (item <= 0)
+                    cont++;
+            }
+
+            Console.WriteLine(cont);
+        }
+    }
+}
---
+++ _ExemploMatrizes/ExercicioMatrix/ExercicioMatrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
