1df84f10fe42f20daaaa2d7d0a492d1a8bf45c44
MatheusEstevesss <matheusesantos@gmail.com>
2021-05-20 15:57:25 -0300

    Adding an example of Enum

---
+++ _ExemplosEnum/ExemploEnum/ExemploEnum/Program.cs
@@ -0,0 +1,28 @@
+﻿using ExemploEnum.Entities;
+using ExemploEnum.Entities.Enums;
+using System;
+
+namespace ExemploEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();// Conversão de enum para string
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");// Conversão string para enum, o texto "Delivered" precisa estar escrito exatamento como no enum
+            Console.WriteLine(txt);
+
+            Console.WriteLine(os);
+        }
+    }
+}
