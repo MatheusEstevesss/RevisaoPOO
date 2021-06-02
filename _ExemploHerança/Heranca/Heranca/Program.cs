c921ef899f09b7e70bdaeee1b47b2a65ae0dd8a0
MatheusEstevesss <matheusesantos@gmail.com>
2021-06-01 23:19:36 -0300

    Adding an example using Inheritance with an attribute protected

---
+++ _ExemploHerança/Heranca/Heranca/Program.cs
@@ -0,0 +1,17 @@
+﻿using Heranca.Entities;
+using System;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            //account.Balance = 200.0  não irá funcionar pois é um atributo protected
+        }
+    }
+}
