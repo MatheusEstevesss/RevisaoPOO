20dbbe855cfe1b76fb8ff6e9b53acebb8207b2c2
MatheusEstevesss <matheusesantos@gmail.com>
2021-06-07 12:45:02 -0300

    Adding a example of Overlay

---
+++ Exemplos/ExemploSobreprosicao/ExSobreposicao/ExSobreposicao/Entities/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpDownCasting.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string  Holder { get; set; }
+        public double Balance { get; set; }
+
+        public Account()
+        {
+
+        }
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public virtual void Withdraw(double amount) //Usa-se a palavra virtual para indicar que o método pode ser sobreposto nas subclasses
+        {
+            Balance -= amount + 5;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
