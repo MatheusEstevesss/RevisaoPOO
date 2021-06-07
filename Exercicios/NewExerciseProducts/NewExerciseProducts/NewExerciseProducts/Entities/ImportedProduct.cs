e8f3f100db69cdb7331cf7a9e1be78c15fa514bc
MatheusEstevesss <matheusesantos@gmail.com>
2021-06-07 14:56:25 -0300

    Adding the new exercise of Products

---
+++ Exercicios/NewExerciseProducts/NewExerciseProducts/NewExerciseProducts/Entities/ImportedProduct.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewExerciseProducts.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+
+        }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double totalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+        public override string priceTag()
+        {
+            return $"{Name} $ {totalPrice()} (Custom fee: $ {CustomsFee})";
+        }
+    }
+}
