fc015ce583b090cdeb4fd66da8fa7d670c7fe0c0
MatheusEstevesss <matheusesantos@gmail.com>
2021-06-07 15:23:43 -0300

    Adding an example with AbstractClass

---
+++ Exemplos/AbstractClass/AbstractClass/AbstractClass/Entities/SavingAccounts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClass.Entities
+{
+    class SavingAccounts : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingAccounts()
+        {
+
+        }
+
+        public SavingAccounts(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {            
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount) //usa-se a palavra override para fazer um sobreposição
+        {
+            base.Withdraw(amount); // executa o método conforme a lógica implementada na superclasse
+            Balance -= 2; // neste caso irá descontar o valor de 5 passado na superclasse e também esse valor de 2, totalizando 7
+        }
+    }
+}
---
+++ Exemplos/AbstractClass/AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using AbstractClass.Entities;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> list = new List<Account>();
+
+            list.Add(new SavingAccounts(1001, "Alex", 500, 0.01));
+            list.Add(new BusinessAccount(1002, "Maria", 500, 400));
+            list.Add(new SavingAccounts(1003, "Bob", 500, 0.01));
+            list.Add(new BusinessAccount(1004, "Anna", 500, 500));
+
+            double sum = 0;
+
+            //Caso a superclasse account não existise, não poderíamos ter essa lista com tipos diferentes, tornando esse cálculo ainda mais dificil de ser executado
+            foreach (Account acc in list)
+            {
+                sum += acc.Balance;
+            }
+
+            Console.WriteLine(sum);
+
+            //Aqui nos aproveitamos do polimorfismo para realizar o método Withdraw em cada conta
+            foreach (Account acc in list)
+            {
+                acc.Withdraw(10);
+            }
+
+            foreach (Account acc in list)
+            {
+                Console.WriteLine(acc.Balance);
+            }
+        }
+    }
+}
